0458e67474522fa34f76fb1c41ca71a8effddb23
sivasai123 <52394434+sivasai123@users.noreply.github.com>
2019-07-17 11:44:19 +0530

    Update a2.py

--- a2.py
+++ a2.py
@@ -1,6 +1,4 @@
-start, end = 1, 10
-# iterating each number in list 
-for num in range(start, end + 1):       
-# checking condition 
+start, end = 1, 10 
+for num in range(start, end + 1):    
 if num % 2 == 0: 
 print(num, end = " ") 
